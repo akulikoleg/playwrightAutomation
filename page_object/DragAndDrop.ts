dff6aacce856d616c70994c24b121a54499b08e1
Oleg Aculov <oaculov@gmail.com>
2024-05-20 11:12:21 -0400

    Drag and drop finished

---
+++ page_object/DragAndDrop.ts
@@ -0,0 +1,54 @@
+import {expect, Page} from "@playwright/test";
+
+export class DragAndDrop {
+  readonly page:Page;
+  constructor(page:Page) {
+    this.page = page;
+  }
+
+  get dragEl(){
+    return '[draggable=\"true\"]';
+  }
+
+  get drophereEl(){
+    return '[id="mydropzone"]';
+  }
+  get droplist(){
+    return '[id="droppedlist"]';
+  }
+
+  get dragMe(){
+    return '[id=\'draggable\']';
+  }
+
+  get dropMe(){
+    return '[id="droppable"]';
+  }
+
+  public async dropAndDropElement(text:string){
+
+   await this.page.locator(this.dragEl, {hasText: text}).dragTo( this.page.locator(this.drophereEl) );
+   expect(await this.page.locator(this.droplist).textContent()).toContain(text);
+
+  }
+
+  public async dropAndDropElementOption2(text:string){
+
+    await this.page.locator(this.dragEl, {hasText: text}).hover();
+    await this.page.mouse.down();
+    await this.page.locator(this.drophereEl).hover();
+    await this.page.mouse.up();
+    expect(await this.page.locator(this.droplist).textContent()).toContain(text);
+
+  }
+  public async dragAndDropElementDemo2(text: string, textInDropBox: string) {
+    await this.page
+      .locator(this.dragMe, { hasText: text })
+      .dragTo(this.page.locator(this.dropMe));
+    expect(await this.page.locator(this.dropMe).textContent()).toContain(textInDropBox);
+    expect(this.page.locator(this.dropMe)).toHaveCSS(
+      "background-color",
+      "rgb(14, 186, 197)"
+    );
+  }
+}
