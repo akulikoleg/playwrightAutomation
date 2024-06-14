ed6dd57847c1762c41921923b18f2c3e2da81a3c
Oleg Aculov <oaculov@gmail.com>
2024-06-13 23:54:01 -0400

    test updated

--- tests/lambdatest/fixtureDragAndDrop.spec.ts
+++ tests/lambdatest/fixtureDragAndDrop.spec.ts
@@ -1,20 +1,20 @@
 import {test, expect} from './dragAndDrop.spec'
 
 
-test('Drag and drop multiple options', async ({page, dragAndDrop}) => {
-
-      await dragAndDrop.dropAndDropElementOption2('Draggable 1');
-      await dragAndDrop.dropAndDropElementOption2('Draggable 2');
-      await dragAndDrop.dragAndDropElementDemo2('Drag me to my target', 'Dropped!');
-      expect( dragAndDrop.dragMe).toContain('Dropped!');
-
-
-
-
-/*   expect(await this.page.locator(this.dropMe).textContent()).toContain(textInDropBox);
-    await expect(await this.page.locator(this.dropMe)).toHaveCSS(
-      "background-color",
-      "rgb(14, 186, 197)"
-    ); */
-
-})
+// test('Drag and drop multiple options', async ({page, dragAndDrop}) => {
+//
+//       await dragAndDrop.dropAndDropElementOption2('Draggable 1');
+//       await dragAndDrop.dropAndDropElementOption2('Draggable 2');
+//       await dragAndDrop.dragAndDropElementDemo2('Drag me to my target', 'Dropped!');
+//       expect( dragAndDrop.dragMe).toContain('Dropped!');
+//
+//
+//
+//
+// /*   expect(await this.page.locator(this.dropMe).textContent()).toContain(textInDropBox);
+//     await expect(await this.page.locator(this.dropMe)).toHaveCSS(
+//       "background-color",
+//       "rgb(14, 186, 197)"
+//     ); */
+//
+// })
