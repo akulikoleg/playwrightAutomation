18e49e9a0295250714e0152412f66f901a4d0ced
Oleg Aculov <oaculov@gmail.com>
2024-06-13 23:41:22 -0400

    yml updated

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
@@ -44,7 +44,7 @@ export default defineConfig({
     {
       name: 'chromium',
       //use: { ...devices['Desktop Chrome'], storageState: './.auth/user.json' },
-      use: { ...devices['Desktop Chrome'], storageState: './.auth/userLocalCoding.json' },
+      use: { ...devices['Desktop Chrome'], headless: true,  storageState: './.auth/userLocalCoding.json' },
      // dependencies: ['setup'],
     },
 
